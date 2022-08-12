863d848f47a9718a432bfc8d65980d84aa43e15d
Vera <moirabestwaifu@gmail.com>
2022-08-12 10:38:26 +0200

    made screen and functioning buttons

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a, b){
+    return +a + +b;
+}
+
+function subtract(a, b){
+    return +a - +b;
+}
+
+function multiply(a, b){
+    return +a * +b;
+}
+
+function divide(a, b){
+    return +a / +b;
+}
+
+function operate(operator, a, b){
+    switch (operator) {
+        case "+":
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        case "/":
+            return divide(a, b);
+            break;
+    }
+}
+
+let screenValue = "";
+let screen = document.querySelector('.screen');
+
+function writeOnScreen(c){
+    let num = document.querySelector(c);
+    screenValue+=num.textContent;
+    screen.textContent = screenValue;
+}
